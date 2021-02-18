2cdd2513763b9b63a4297b016d5b410678aebd45
scalahub <23208922+scalahub@users.noreply.github.com>
2021-02-18 22:14:33 +0530

    Add scripts for StableCoin demo instance
    Mint SC
    Mint RC
    Redeem RC
    Get SC info
    Get RC info

---
+++ getStableCoinInfoCLI.sh
@@ -0,0 +1,1 @@
+java -jar target/scala-2.12/JDE-assembly-0.1.jar src/test/resources/AgeUSD/exchange/getStableCoinInfo.json
---
+++ getStableCoinInfoWeb.sh
@@ -0,0 +1,2 @@
+curl --data @src/test/resources/AgeUSD/exchange/getStableCoinInfo.json http://localhost:8080/compile
+
